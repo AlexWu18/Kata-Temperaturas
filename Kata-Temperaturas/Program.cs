ea8ddeb43e015ac7e8e50c7d3519b8a463bb64aa
AlexWu18 <alejandrowu189@gmail.com>
2022-05-27 00:10:57 -0400

    Add project files.

---
+++ Kata-Temperaturas/Program.cs
@@ -0,0 +1,17 @@
+﻿static double CelciusAFahren (double TemperaCelc) // convert celcius to Fahrenheit
+{
+    double TemperaFah = 0;
+    TemperaFah = (TemperaCelc * 9) / 5 + 32; // convert formula
+    return TemperaFah;
+}
+
+static double FahrenACelcius (double Fahrenheit) // Fahrenheit to Celcius
+{
+    return (Fahrenheit - 32) * 5 / 9; //Formula
+}
+
+static double CelciusAKelvin (double celcius) // Celcius to Kelvin
+{
+    return (celcius + 273.15); // formula
+}
+
