a4f406b74628e10f72ca90f8e9957e730ede1340
AlexWu18 <alejandrowu189@gmail.com>
2022-05-27 00:25:10 -0400

    Commit 2

--- Kata-Temperaturas/Program.cs
+++ Kata-Temperaturas/Program.cs
@@ -15,3 +15,22 @@ static double CelciusAKelvin (double celcius) // Celcius to Kelvin
     return (celcius + 273.15); // formula
 }
 
+Console.Write("Introducir el valor de la temperatura en Celcius(C) para Fahrenheit: "); //ask
+double celtemp = Convert.ToDouble(Console.ReadLine());// capture the values from the console Celcius
+
+
+Console.Write("Introducir el valor de la temperatura en Fahrenheit(F) para Celcius: "); //ask
+double Fahrenheit = Convert.ToDouble(Console.ReadLine()); //capture values from fahrenheit
+
+Console.Write("Introducir el valor de la temperatura en Celcius(C) para Kelvin: "); //ask
+double Kelvin = Convert.ToDouble(Console.ReadLine()); //capture values Celcius to Kelvin
+
+
+double ferTemp = CelciusAFahren(celtemp);
+Console.WriteLine("La conversion de Celcius A Fahrenheit es : " + ferTemp);
+
+
+                                                      
+
+
+
